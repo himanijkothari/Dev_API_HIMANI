72ec1c8529c2b64fc43cd776dbeed21e8a509076
Himani Paronigar <himanijkothari@gmail.com>
2021-04-11 21:49:07 +0200

    commiting exercise-1

---
+++ src/main/java/se/dev/api/Article.java
@@ -0,0 +1,70 @@
+/**
+ * Entity that represents the article
+ * @authorname: Himani Paronigar
+ */
+
+package se.dev.api;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Article {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String body;
+
+    private String authorName;
+
+    public  Article()
+    {
+
+    }
+
+    public Article(String title, String body, String authorName) {
+        this.title = title;
+        this.body = body;
+        this.authorName = authorName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
